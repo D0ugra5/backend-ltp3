cda01756702c7d391ec1f6a0a15783b50f3942a2
Douglas Sousa <douglasmb87@gmail.com>
2022-11-22 17:25:18 -0300

    2323

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ app = flask.Flask(__name__)
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/favicon.png')
+    return '22223'
 
 
 @app.route('/')
